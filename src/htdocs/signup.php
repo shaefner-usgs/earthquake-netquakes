dd19ac1516bc5b9551923d72e04c137602527645
Scott Haefner <shaefner@usgs.gov>
2017-01-24 15:37:40 -0800

    add map;

--- src/htdocs/signup.php
+++ src/htdocs/signup.php
@@ -6,10 +6,21 @@ include_once '../lib/classes/Db.class.php'; // db connector, queries
 if (!isset($TEMPLATE)) {
   $TITLE = 'NetQuakes Sign Up';
   $NAVIGATION = true;
-  $HEAD = '';
-  $FOOT = '';
+  $HEAD = '
+    <link rel="stylesheet" href="/lib/leaflet-0.7.7/leaflet.css" />
+    <link rel="stylesheet" href="css/signup.css" />
+  ';
+  $FOOT = '
+    <script>
+      var MOUNT_PATH = "' . $MOUNT_PATH . '";
+    </script>
+    <script src="/lib/leaflet-0.7.7/leaflet.js"></script>
+    <script src="js/signup.js"></script>
+  ';
 
   include 'template.inc.php';
 }
 
 ?>
+
+<div class="map"></div>
