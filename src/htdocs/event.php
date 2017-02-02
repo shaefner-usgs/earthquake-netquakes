71ec3e163bf93b266d3ce0ce1cb29f6f82d5ee50
Scott Haefner <shaefner@usgs.gov>
2017-02-01 16:25:08 -0800

    initial commit

---
+++ src/htdocs/event.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once '../conf/config.inc.php'; // app config
+include_once '../lib/_functions.inc.php'; // app functions
+include_once '../lib/classes/Db.class.php'; // db connector, queries
+
+$event = safeParam('event');
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'NetQuakes Data';
+  $NAVIGATION = true;
+  $HEAD = '<link rel="stylesheet" href="../css/event.css" />';
+  $FOOT = '';
+
+  include 'template.inc.php';
+}
+
+$db = new Db;
+
+// Db query results: Plots
+$rsPlots = $db->queryPlots($event);
+
+// Create HTML for plot thumbnails
+$plotsHtml = '<ul class="no-style seismograms">';
+while ($row = $rsPlots->fetch(PDO::FETCH_ASSOC)) {
+  $plotsHtml .= sprintf('<li>
+      <h4>%s_%s_%s</h4>
+      <p>%s km from epicenter</p>
+      <a href="%s/%s">
+        <img src="%s/data/trigs/tn-%s" alt="NetQuakes seismogram" />
+      </a>
+    </li>',
+    $row['site'],
+    $row['net'],
+    $row['loc'],
+    $row['evtdst'],
+    $event,
+    preg_replace('/[-: ]/', '', $row['datetime']),
+    $MOUNT_PATH,
+    $row['file']
+  );
+  if ($row['evtmag']) {
+    $mag = $row['evtmag'];
+  }
+  if ($row['evttime']) {
+    $time = $row['evttime'];
+  }
+}
+$plotsHtml .= '</ul>';
+
+$subTitle = sprintf('M %s Earthquake - %s UTC',
+  $mag,
+  date ('M j, Y H:i:s', $time)
+);
+
+print "<h2>$subTitle</h2>";
+
+print $plotsHtml;
---
+++ src/htdocs/instrument.php
@@ -0,0 +1,62 @@
+<?php
+
+include_once '../conf/config.inc.php'; // app config
+include_once '../lib/_functions.inc.php'; // app functions
+include_once '../lib/classes/Db.class.php'; // db connector, queries
+
+$instrument = safeParam('instrument');
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'NetQuakes Data';
+  $NAVIGATION = true;
+  $HEAD = '<link rel="stylesheet" href="../css/instrument.css" />';
+  $FOOT = '';
+
+  include 'template.inc.php';
+}
+
+$db = new Db;
+
+// Db query results: Plots
+$rsPlots = $db->queryPlots($instrument);
+
+$types = [
+  'TRG' => 'Triggered Events',
+  'USR' => 'System Requested Data',
+  'CAL' => 'Calibration Pulses'
+];
+
+// Create associative array w/ plots indexed by type (first column of result set)
+$plots = $rsPlots->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
+
+// Create HTML for plot thumbnails
+$plotsHtml = '';
+foreach ($types as $key=>$value) {
+  if (array_key_exists($key, $plots)) { // not all types nec. included
+    if ($plotsHtml) {
+      $plotsHtml .= '</ul>'; // close open <ul> tag if it exists
+    }
+    $plotsHtml .= "<h3>$value</h3>";
+    $plotsHtml .= '<ul id="' . strtolower($key) . '" class="no-style seismograms">';
+    foreach ($plots[$key] as $plot) {
+      $description = trim($plot['description']);
+      $plotsHtml .= sprintf('<li>
+          <h4>%s UTC</h4>
+          <a href="%s/%s">
+            <img src="%s/data/trigs/tn-%s" alt="NetQuakes seismogram" />
+          </a>
+        </li>',
+        date('M j, Y H:i:s', $plot['unixtime']),
+        $instrument,
+        preg_replace('/[-: ]/', '', $plot['datetime']),
+        $MOUNT_PATH,
+        $plot['file']
+      );
+    }
+  }
+}
+$plotsHtml .= '</ul>';
+
+print "<h2>$instrument ($description)</h2>";
+
+print $plotsHtml;
