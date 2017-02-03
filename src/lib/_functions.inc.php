ecedf6164c457e7d6f0a91c2ccea57c128233456
Scott Haefner <shaefner@usgs.gov>
2017-02-03 13:56:31 -0800

    add method and move statelist to separate file

--- src/lib/_functions.inc.php
+++ src/lib/_functions.inc.php
@@ -1,68 +1,40 @@
 <?php
 
 include_once('_geocode.inc.php');
+include_once('_getStateList.inc.php');
 
-/*
- * Get list of U.S. states for pulldown menu
- *
- * @return {Html}
+/**
+ * Determine lat/lng bounds of each region and add them to the array
  */
-function getStateList () {
-  return '
-    <option value=""></option>
-    <option value="AL">Alabama</option>
-    <option value="AK">Alaska</option>
-    <option value="AZ">Arizona</option>
-    <option value="AR">Arkansas</option>
-    <option value="CA">California</option>
-    <option value="CO">Colorado</option>
-    <option value="CT">Connecticut</option>
-    <option value="DE">Delaware</option>
-    <option value="DC">District of Columbia</option>
-    <option value="FL">Florida</option>
-    <option value="GA">Georgia</option>
-    <option value="HI">Hawaii</option>
-    <option value="ID">Idaho</option>
-    <option value="IL">Illinois</option>
-    <option value="IN">Indiana</option>
-    <option value="IA">Iowa</option>
-    <option value="KS">Kansas</option>
-    <option value="KY">Kentucky</option>
-    <option value="LA">Louisiana</option>
-    <option value="ME">Maine</option>
-    <option value="MD">Maryland</option>
-    <option value="MA">Massachusetts</option>
-    <option value="MI">Michigan</option>
-    <option value="MN">Minnesota</option>
-    <option value="MS">Mississippi</option>
-    <option value="MO">Missouri</option>
-    <option value="MT">Montana</option>
-    <option value="NE">Nebraska</option>
-    <option value="NV">Nevada</option>
-    <option value="NH">New Hampshire</option>
-    <option value="NJ">New Jersey</option>
-    <option value="NM">New Mexico</option>
-    <option value="NY">New York</option>
-    <option value="NC">North Carolina</option>
-    <option value="ND">North Dakota</option>
-    <option value="OH">Ohio</option>
-    <option value="OK">Oklahoma</option>
-    <option value="OR">Oregon</option>
-    <option value="PA">Pennsylvania</option>
-    <option value="RI">Rhode Island</option>
-    <option value="SC">South Carolina</option>
-    <option value="SD">South Dakota</option>
-    <option value="TN">Tennessee</option>
-    <option value="TX">Texas</option>
-    <option value="UT">Utah</option>
-    <option value="VT">Vermont</option>
-    <option value="VA">Virginia</option>
-    <option value="WA">Washington</option>
-    <option value="DC">Washington D.C.</option>
-    <option value="WV">West Virginia</option>
-    <option value="WI">Wisconsin</option>
-    <option value="WY">Wyoming</option>
-  ';
+function getRegions ($items) {
+  $regions = [];
+
+  foreach ($items as $item) {
+    $coordPairs = json_decode($item['coords']);
+    $region = [ // hang on to these elements
+      'name' => $item['name'],
+      'code' => $item['code'],
+      'contacts' => $item['contacts'],
+      'message' => $item['message']
+    ];
+
+    if ($coordPairs) {
+      $lats = [];
+      $lons = [];
+
+      foreach ($coordPairs as $coordPair) {
+        array_push($lats, $coordPair[0]);
+        array_push($lons, $coordPair[1]);
+      }
+      $region['n'] = max($lats);
+      $region['e'] = max($lons);
+      $region['s'] = min($lats);
+      $region['w'] = min($lons);
+    }
+    array_push($regions, $region);
+  }
+
+  return $regions;
 }
 
 /**
